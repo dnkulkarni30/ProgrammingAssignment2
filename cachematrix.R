c52f0f01d5137794cf88b489c58f3dc9cb4f79d0
dnkulkarni30 <dnkulkarni30@live.com>
2016-08-19 12:30:02 +0530

    this is function to cache the inverse of hte matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is pair functions one to createa a matrix in a way to be used later for caching. The other function 
+## then can cache the inverse of same and returns inverse.
 
-## Write a short comment describing this function
+
+## This function creates matrix and returns a list of functions using which we can see the content
+## of the matrix and also see the inverse of the matrix(if already calculate)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        invX <- NULL
+       
+        #function 1 to populate matrix data.
+        set <- function(y) 
+          {
+              x <<- y    
+              invX <<- NULL 
+          }
+        
+        #function 2 to get the matrix data  
+        get <- function() x
+        
+        #function 3 to set the inverse of the matrix with supplied values
+        setInverse <- function(inv) invX <<- inv 
+        
+        #function 4 to get the inverse of the matrix in return.
+        getInverse <- function() invX
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function is to cache the inverse of a matrix and use it whenever call is made to this function.
+##It assumes that the matrix passed is a square matrix and is invertible
+cacheSolve <- function(x, ...) 
+{
+        invX <- x$getInverse()
+        if(!is.null(invX)) {
+          message("getting cached matrix inversion data")
+          return(invX)  #return and abort.
+        }
+        data <- x$get()
+        invX <- solve(data)
+        x$setInverse(invX)
+        invX #return
 }
+
